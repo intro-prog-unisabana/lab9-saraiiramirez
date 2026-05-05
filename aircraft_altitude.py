03a5c85727460981c2c4cdb849f77231a4dacb9e
Sarai Ramirez <sarairace@unisabana.edu.co>
2026-05-04 21:25:36 -0500

    lab 9

--- aircraft_altitude.py
+++ aircraft_altitude.py
@@ -1,34 +1,23 @@
-
 from aircraft import Aircraft
-
 
 modelo = input("Enter aircraft model: ")
 mi_avion = Aircraft(modelo)
 
-
 while True:
-
     comando_usuario = input("Enter command (A for ascent, D for descent, X to exit): ")
-    
     
     if comando_usuario.upper() == 'X':
         break
     
-
     partes = comando_usuario.split()
     
-
     if len(partes) == 2:
-        accion = partes[0].upper() 
-        pies = int(partes[1])      
-        
+        accion = partes[0].upper()
+        pies = int(partes[1])
         
         if accion == 'A':
-          
-            mi_avion.ascend(pies) 
+            mi_avion.climb(pies)
         elif accion == 'D':
-       
             mi_avion.descend(pies)
-
 
 print(f"Final altitude: {mi_avion.altitude} feet")
--- movie.py
+++ movie.py
@@ -9,14 +9,3 @@ class Movie:
         
         return f"Movie: {self.title} (Directed by {self.director}, {self.year})"
 
-
-
-titulo_input = input("Enter title: ")
-director_input = input("Enter director: ")
-years_input = input("Enter year: ")
-
-
-mi_pelicula = Movie(titulo_input, director_input, years_input)
-
-
-print(mi_pelicula)
