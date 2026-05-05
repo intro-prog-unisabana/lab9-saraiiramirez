3fb32f4ed9b2f1812ea4948357baaf6c345b12bb
Sarai Ramirez <sarairace@unisabana.edu.co>
2026-05-04 20:20:55 -0500

    lab 9

--- utils.py
+++ utils.py
@@ -1,1 +1,29 @@
 # utils.py
+from person import Person
+from bank_account import BankAccount
+
+
+def person_data():
+    name = input("Enter the person's name:\n")
+    person = Person(name)
+
+    done = "no"
+
+    while done != "yes":
+        account_number = int(input("Enter a 4-digit account number:\n"))
+        balance = float(input("Enter the initial balance:\n"))
+
+        account = BankAccount(account_number, balance)
+        person.add_account(account)
+
+        done = input("Are you done adding accounts? (yes/no):\n")
+
+    return person
+
+
+def balance_summary(person_list):
+    for person in person_list:
+        total_balance = 0
+        for account in person.accounts:
+            total_balance += account.balance
+        print(f"{person.name} : {total_balance:.2f}")
